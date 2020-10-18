196d5d3fc37f6b42f024a18838fd155da799d14a
Andrei Mackenzie <andrei@clypd.com>
2020-10-18 14:55:52 -0400

    Include webpack style loader

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from "react-dom";
 import Viewer from "./js/components/Viewer";
 import r from './js/recipes/recipes.json';
 
+import "./styles.css";
+
 const wrapper = document.getElementById("container");
 wrapper ? ReactDOM.render(<Viewer recipes={r.recipes} />, wrapper) : false;
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,13 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
