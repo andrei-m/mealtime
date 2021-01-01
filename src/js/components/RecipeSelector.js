f082bb6be3a590d6993d16a06921e3e5c6dde413
Andrei Mackenzie <andrei@clypd.com>
2021-01-01 16:40:31 -0500

    Define a way to select recipes to add to the viewer
    The next step is a 'multi-viewer' that will render all selected recipes
    instead of just the first recipe.

---
+++ src/js/components/RecipeSelector.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class RecipeSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOrdinal: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({selectedOrdinal: parseInt(event.target.value)});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.selectionHandler(this.state.selectedOrdinal);
+  }
+
+  render() {
+    var options = [];
+    for (var i = 0; i < this.props.recipes.length; i++) {
+      options.push(<option value={i}>{this.props.recipes[i].name}</option>);
+    }
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <select value={this.state.value} onChange={this.handleChange}>
+          {options}
+        </select>
+        <input type="submit" value="+Add" />
+      </form>
+    );
+  }
+}
+
+export default RecipeSelector;
--- src/js/components/Viewer.js
+++ src/js/components/Viewer.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Recipe from './Recipe';
+import RecipeSelector from './RecipeSelector';
 import Timer from './Timer';
 
 class Viewer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenOrdinals: []
+    };
+
+    this.selectionHandler = this.selectionHandler.bind(this);
+  }
+
+  selectionHandler(ordinal) {
+    if (!this.state.chosenOrdinals.includes(ordinal)) {
+      var appended = this.state.chosenOrdinals.concat(ordinal);
+      this.setState({chosenOrdinals: appended});
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="menu">
           <Timer />
+          
+          <div>
+            <RecipeSelector recipes={this.props.recipes} selectionHandler={this.selectionHandler} />
+          </div>
         </div>
         <Recipe recipe={this.props.recipes[0]} />
       </div>
